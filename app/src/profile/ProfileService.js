7c049aafbd9a4a12f866a8d20a5465cf5618eb6a
Vamsi Krishna Gajula <gvamsikris@gmail.com>
2016-03-29 18:22:53 +0530

    Skills and work exp

---
+++ app/src/profile/ProfileService.js
@@ -0,0 +1,77 @@
+// Profile Service
+(function(){
+	'use strict';
+	angular.module('profile').service('ProfileService', [ProfileService]);
+
+	function ProfileService() {
+		var profileService = {};
+		profileService.loadProfile = function() {
+			return {
+				summary: {
+					name: 'Vamsi Krishna Gajula',
+					designation: 'Senior Applications Engineer @ Oracle'
+				},
+				skills: [
+					{
+						label: 'Java',
+						level: 8
+					},
+					{
+						label: 'Spring',
+						level: 8
+					},
+					{
+						label: 'JavaScript',
+						level: 8
+					},
+					{
+						label: 'Groovy',
+						level: 6
+					}
+				],
+				work: [
+					{
+						company: 'Oracle',
+						role: 'Senior Applications Engineer',
+						start: 'July 2015',
+						end: 'Present' 
+					},
+					{
+						company: 'Schneider Electric',
+						role: 'Senior Software Engineer',
+						start: 'September 2011',
+						end: 'July 2015' 
+					},
+					{
+						company: 'Tata Consultancy Services',
+						role: 'Software Engineer',
+						start: 'August 2008',
+						end: 'September 2011' 
+					}
+				],
+				education: [
+					{
+						school: 'VIT University',
+						degree: 'Bachelor of Technology',
+						start: 'July 2004',
+						end: 'May 2008',
+						percentage: '8.43/10' 
+					},
+					{
+						company: 'Schneider Electric',
+						role: 'Senior Software Engineer',
+						start: 'September 2011',
+						end: 'July 2015' 
+					},
+					{
+						company: 'Tata Consultancy Services',
+						role: 'Software Engineer',
+						start: 'August 2008',
+						end: 'September 2011' 
+					}
+				]
+			}
+		};
+		return profileService;
+	}
+})();
